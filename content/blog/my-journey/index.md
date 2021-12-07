458c583e4494e6a1ecd9caa58905b07f62c3d0e1
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2021-12-07 11:22:36 +0200

    first commit

---
+++ content/blog/my-journey/index.md
@@ -0,0 +1,16 @@
+---
+title: Who Am I
+date: "2021-12-06T22:12:03.284Z"
+description: "Allow me to introduce myself"
+---
+
+**Let me get the boring stuff out of the way first:
+ My name is Kabelo Maswanganye and I live in Pretoria. 
+I am a self-taught developer who started coding during the Covid-19 lockdown of 2020.**
+
+The reason I started coding was literally boredom. I was bored out of my skull and needed something to keep me busy. After I had burned through all the seasons of Game Of Thrones and watched the entire Harry Potter series twice, I needed something that was productive. That is when Facebook somehow read my mind and started advertising coding bootcamps to me. 
+
+This lead me down a deep Google and YouTube rabbit hole that ended with me printing my first "Hello World" in python. That feeling was amazing. Very exciting. For the next few months, all my free time was spent working on python. Time really does fly while coding.
+
+Web Development only crossed my radar towards the end of 2020. I was on twitter looking for topics to follow and realised that most of Tech Twitter was mostly web development content, so I gave it shot. I found a FreeCodeCamp HTML and CSS YouTube video and followed it. Now if printing Hello World in python was exciting, building my first website was exhilarating. It was as ugly as hell, but I didn't care. I loved it. I loved it so much I probably sent all my WhatsApp contacts a screenshot. Bragging about my new skill.
+
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Hills And Valleys`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Kabelo Maswanganye`,
+      summary: `who lives and works in Pretoria, South Africa.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `A blog where I share my coding ups and downs`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `KabeloM06`,
     },
   },
   plugins: [
@@ -46,6 +46,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `gatsby-remark-reading-time`,
         ],
       },
     },
@@ -95,6 +96,10 @@ module.exports = {
                     html
                     fields {
                       slug
+                      readingTime {
+                        text
+                        minutes
+                      }
                     }
                     frontmatter {
                       title
@@ -113,15 +118,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Hills And Valleys`,
+        short_name: `HAV`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
